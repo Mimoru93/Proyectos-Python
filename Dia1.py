156d3f236866908816f8b8b28cd560ef229b8079
Miriam Moreira <mimoru93@gmail.com>
2025-02-13 12:03:48 +0100

    Generador de nombres de bandas

---
+++ Dia1.py
@@ -0,0 +1,6 @@
+#Creación de un programa para generar nombres de bandas
+
+print("Bienvenido al generador de nombres de bandas")
+city = input("Donde naciste?\n")
+pet_name = input("¿Cuál es el nombre de tu mascota?\n")
+print ("El nombre de la banda podría ser", city, pet_name)
